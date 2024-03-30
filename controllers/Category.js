76cd9c076c6e03654aaf97016cf5f4b83c0c884d
rohitshaw-10 <rohitshaw3622@gmail.com>
2024-03-30 14:07:32 +0530

    fimal

--- controllers/Category.js
+++ controllers/Category.js
@@ -31,13 +31,15 @@ exports.createCategory = async (req, res) => {
 
 exports.showAllCategories = async (req, res) => {
 	try {
-        console.log("INSIDE SHOW ALL CATEGORIES");
+    console.log("INSIDE SHOW ALL CATEGORIES");
 		const allCategorys = await Category.find({});
 		res.status(200).json({
 			success: true,
 			data: allCategorys,
 		});
 	} catch (error) {
+    console.log("INSIDE SHOW ALL CATEGORIES giving error ", error);
+    console.log("INSIDE SHOW ALL CATEGORIES giving error ", error);
 		return res.status(500).json({
 			success: false,
 			message: error.message,
